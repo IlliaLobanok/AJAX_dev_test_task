fe8c417293648f5bda2c939b614c91dc77cdb5b1
IlliaLobanok <i.l.lobanok@gmail.com>
2023-09-08 01:53:53 +0300

    + test for finding button, search by xpath + test for clicking the button (success condition = email box is found)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
+from appium.webdriver.common.touch_action import TouchAction
 
 
 @pytest.fixture()
@@ -27,17 +28,30 @@ def test_connection(get_driver_ajaxsystems):
 
 def test_find_button(get_driver_ajaxsystems):
     driver = get_driver_ajaxsystems
-    button = driver.find_element(by=AppiumBy.XPATH, value="//*[@text = 'Log In']")
+    try:
+        log_in_button = driver.find_element(by=AppiumBy.XPATH, value="//*[@text = 'Log In']")
+    except Exception as e:
+        pytest.skip(f"Appium error: {e}")
     assert True
 
-# class TestAppium(unittest.TestCase):
-#     def setUp(self) -> None:
-#         self.driver = webdriver.Remote(appium_server_url, capabilities)
-#
-#     def tearDown(self) -> None:
-#         if self.driver:
-#             self.driver.quit()
-#
-#     def test_find_wifi(self) -> None:
-#         el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Wi-Fi"]')
-#         el.click()
+
+def test_click_button(get_driver_ajaxsystems):
+    driver = get_driver_ajaxsystems
+    try:
+        log_in_button = driver.find_element(by=AppiumBy.XPATH, value="//*[@text = 'Log In']")   # find the button
+    except Exception as e:
+        pytest.skip(f"Appium error: {e}")
+
+    action = TouchAction(driver)
+    try:
+        action.tap(log_in_button).perform()     # tap the button
+    except Exception as e:
+        pytest.skip(f"Appium error: {e}")
+
+    try:
+        email_box = driver.find_element(by=AppiumBy.XPATH, value="//*[@text = 'Email']")    # look for an email box
+    except Exception as e:
+        pytest.skip(f"Appium error: {e}")       # if there's no such thing, end the test
+    assert True                                 # executed only if no exceptions were raised (object found)
+
+
