aeb7afb294320d71f88ea36104e2558470f6ebd0
IlliaLobanok <i.l.lobanok@gmail.com>
2023-09-08 18:11:02 +0300

    + log_in method of LoginPage w/ credentials optional parameters + test of log in changed corresponding to the new method (positive case only)

--- framework/login_page.py
+++ framework/login_page.py
@@ -1,4 +1,6 @@
 from .page import Page
+from appium.webdriver.common.appiumby import AppiumBy
+from typing import Optional
 
 
 class LoginPage(Page):
@@ -7,5 +9,45 @@ class LoginPage(Page):
         self.email = "qa.ajax.app.automation@gmail.com"
         self.password = "qa_automation_password"
 
-    def log_in(self):
-        pass
+    def log_in(self, email: Optional[str] = None, password: Optional[str] = None):
+        if email is None:
+            email = self.email
+        if password is None:
+            password = self.password
+
+        button_log_in = self.find_elements("//*[@text = 'Log In']")
+        if button_log_in is None:
+            return "ERROR at log_in(): starting button_log_in not found"
+        self.tap_element(button_log_in[0])
+
+        credents_boxes = self.find_elements('android.widget.EditText', AppiumBy.CLASS_NAME)
+        if credents_boxes is None:
+            return "ERROR at log_in(): credents_boxes not found"
+        self.tap_element(credents_boxes[0])
+        self.enter_text(email, credents_boxes[0])
+        self.tap_element(credents_boxes[1])
+        self.enter_text(password, credents_boxes[1])
+
+        view_elements = self.find_elements("android.view.View", AppiumBy.CLASS_NAME)
+        if view_elements is None:
+            return "ERROR at log_in(): view_elements not found"
+        eye_icon = None
+        for element in view_elements:
+            if element.get_attribute("resource-id") == "iconPassword":
+                eye_icon = element
+        if eye_icon is not None:
+            self.tap_element(eye_icon)
+        else:
+            return "ERROR at log_in(): eye_icon not found"
+        if not credents_boxes[0].text == email and credents_boxes[1].text == password:
+            return "ERROR at log_in(): credents_boxes test values do not equal desired ones"
+
+        button_log_in = self.find_elements("//*[@text = 'Log In']")
+        if button_log_in is None:
+            return "ERROR at log_in(): ending button_log_in not found"
+        self.tap_element(button_log_in[0])
+        return "OK"
+
+
+
+
--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -2,6 +2,7 @@ from framework import LoginPage
 import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
+from framework import LoginPage
 
 
 @pytest.fixture
@@ -30,29 +31,6 @@ def test_user_login(user_login_fixture):
     current_page = user_login_fixture
     assert current_page.driver.current_context == 'NATIVE_APP'
 
-    button_log_in = current_page.find_elements("//*[@text = 'Log In']")
-    current_page.tap_element(button_log_in[0])
-    credents_boxes = current_page.find_elements('android.widget.EditText', AppiumBy.CLASS_NAME)
-    assert credents_boxes is not None
-
-    current_page.tap_element(credents_boxes[0])
-    current_page.enter_text(current_page.email, credents_boxes[0])
-
-    current_page.tap_element(credents_boxes[1])
-    current_page.enter_text(current_page.password, credents_boxes[1])
-
-    view_elements = current_page.find_elements("android.view.View", AppiumBy.CLASS_NAME)
-    assert view_elements is not None
-    eye_icon = None
-    for element in view_elements:
-        if element.get_attribute("resource-id") == "iconPassword":
-            eye_icon = element
-    if eye_icon is not None:
-        current_page.tap_element(eye_icon)
-    else:
-        pytest.skip("Unable to find eye icon element")
-
-    assert credents_boxes[0].text == current_page.email
-    assert credents_boxes[1].text == current_page.password
-
-
+    result_login_sequence = current_page.log_in()
+    if result_login_sequence is not None:
+        pytest.skip(result_login_sequence)
